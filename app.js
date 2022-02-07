01926136d9307be943d57e32891835924e3af8c6
Difelice-e <difelice.emanuele@gmail.com>
2022-02-07 14:49:29 +0100

    completamento esercizio

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const container = document.getElementById("main_container")
-console.log(container)
+const container = document.getElementById("main_container");
+console.log(container);
 
 for (let i = 1; i < 101; i++) {
     const squareWrapper = document.createElement("div");
     container.append(squareWrapper);
-    squareWrapper.classList.add("number_wrapper")
-    squareWrapper.append(i)
-
+    squareWrapper.classList.add("number_wrapper");
+    
     console.log(i)
+    if (i % 15 === 0) {
+        squareWrapper.classList.add("fizzbuzz_wrapper")
+        squareWrapper.append("fizzbuzz");
+    }
+        else if (i % 3 === 0) {
+            squareWrapper.classList.add("fizz_wrapper")
+            squareWrapper.append("fizz");
+            
+        } else if (i % 5 === 0) {
+            squareWrapper.classList.add("buzz_wrapper")
+            squareWrapper.append("buzz");
+        } else {
+            squareWrapper.append(i)
+        }
+  
 }
+
